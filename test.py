a63505b44a9ea1165c720e45eaf02e6d0b99db6b
ljfbleach <ljfbleach@163.com>
2018-01-07 18:09:44 +0800

    Python test

---
+++ test.py
@@ -0,0 +1,14 @@
+if True:
+	print ("Hello, Hebe!")
+else:
+ 	print ("Hello, My Love")
+
+#raw_input("\nIt's to Hebe!\n")
+
+counter = 100
+miles = 1000.00
+name = "Tomorrow is another day"
+
+print (counter)
+print (miles)
+print (name[1:7])
